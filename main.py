0fba7db975a44bc7b6fcd90906a1c4cd28adee56
Kaike De Souza <kaikedesz@gmail.com>
2026-05-25 18:41:23 -0300

    Fix docstring formatting in verificar_par_impar

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def verificar_par_impar(n1, n2):
-    # Verifica se a soma de dois números é par ou ímpar.
+    """Verifica se a soma de dois números é par ou ímpar."""
     if (n1 + n2) % 2 == 0:
-        return  "Par"
+        return "Par"
     return "Impar"
+
 
 if __name__ == "__main__":
     print(verificar_par_impar(2, 2))
+    
