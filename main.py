b8378ade4dd94dbb4b8e399d77374a6490970383
Kaike De Souza <kaikedesz@gmail.com>
2026-05-25 18:43:14 -0300

    Add docstring for verifying even or odd sum
    Add docstring to verificar_par_impar function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def verificar_par_impar(n1, n2):
+    
     """Verifica se a soma de dois números é par ou ímpar."""
     if (n1 + n2) % 2 == 0:
         return "Par"
     return "Impar"
-
 
 if __name__ == "__main__":
     print(verificar_par_impar(2, 2))
