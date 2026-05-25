028515435e3574690b0332ad387735a4f0bd36e8
Kaike De Souza <kaikedesz@gmail.com>
2026-05-25 18:33:08 -0300

    Fix function name in tests for parity verification

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,10 @@ from main import verificar_par_impar
 
 
 def test_numero_par():
-    assert verifica_par(2, 2) == "Par"
+    assert verificar_par_impar(2, 2) == "Par"
 
 
 def test_numero_impar():
-    assert verifica_par(1, 2) == "Impar"
+    assert verificar_par_impar(1, 2) == "Impar"
 
 #Fim
